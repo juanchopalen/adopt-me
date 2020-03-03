0b8218433c7d014538d13f6eb3314af2adf65dd5
Juan Palencia <juan.palencia@majesticmedia.ca>
2020-03-03 10:02:07 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Teo",
+      animal: "Dog",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, {
+      name: "Luca",
+      animal: "Cat",
+      breed: "Cacri"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
